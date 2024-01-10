d9fa38d0e4fb46c5466f3b7d931be9fe729b1e73
AbduselamNur <abduselamnurhussen626@gmail.com>
2024-01-10 22:35:05 +0300

    Refactor queryAPI function to use const and increment count by 1

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  let count = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, ++count);
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
 
   if (count >= 5) {
     throw new Error('Endpoint load is high');

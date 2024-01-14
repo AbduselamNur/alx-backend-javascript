551866fa6a828ce444a736a6ab91aa0fb87d5ebd
AbduselamNur <abduselamnurhussen626@gmail.com>
2024-01-14 10:28:09 +0300

    Writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,10 +15,35 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+export const printTeacher: printTeacherFunction = (firstName, lastName) => {
   const initial = firstName.charAt(0).toUpperCase();
   const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
   return `${initial}. ${fullLastName}`;
 };
 
-export default printTeacher;
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
